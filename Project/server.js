73200eed6cac7f99663ddedd66ec388c1493cc32
Mxrcxsz <74399276+Mxrcxsz@users.noreply.github.com>
2026-03-04 15:55:30 +0800

    did simple user sign up & login

--- Project/server.js
+++ Project/server.js
@@ -1,14 +1,32 @@
-const express = require("express");
-const server = express();
 const path = require("path");
+const express = require("express");
 
-server.use("/", express.static(path.join(__dirname, "public")))
-server.use(express.urlencoded({ extended: true }));
+const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+const auth = require("./routes/auth");
+const movie = require("./routes/movie");
+const review = require("./routes/review");
+app.use("/", auth);
+app.use("/", movie);
+app.use("/", review);
+
+app.get("/", (req, res) => {
+  res.send(`
+    <h1>Movies Watchlist</h1>
+    <a href="signup">Sign Up</a>
+    <a href="login">Login</a>
+  `);
+});
 
 const hostname = "127.0.0.1";
 const port = 8000;
 
-server.listen(port, hostname, () => {
+app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
